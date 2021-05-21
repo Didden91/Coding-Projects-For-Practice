fa47504de0777c19ecd789173681eaec0a1c08d3
Didden91 <81488820+Didden91@users.noreply.github.com>
2021-05-21 21:44:33 +0200

    Create testding.py

---
+++ testding.py
@@ -0,0 +1,6 @@
+inp = input("Enter word, or a \".\" to indicate that you are finished: ")
+print('you entered:', inp)
+
+score = 10
+totalscore = 50
+print("\"%s\" earned %d points. Total: %d points") % (inp, score, totalscore)
